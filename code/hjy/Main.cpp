2d9f824f46fa95d0d1f913ab1aae5e45cd618d26
Calculus9 <1441211576@qq.com>
2021-12-25 01:18:15 +0800

    12.25 test

---
+++ code/hjy/Main.cpp
@@ -0,0 +1,20 @@
+#include "lab_1.h"
+#include "lab2.h"
+int main()
+{
+    int es;
+    es = lexicalAnalysis();
+    if (es > 0)
+        cout << "词法分析有问题, 编译停止!" << endl;
+    else
+        cout << "词法分析成功" << endl;
+    if (es == 0)
+    {
+        es = TESTparse(); //调语法分析
+        if (es == 0)
+            printf("语法分析成功!\n");
+        else
+            printf("语法分析错误!\n");
+    }
+    return 0;
+}
