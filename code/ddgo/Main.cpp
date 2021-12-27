2cd419d6e8f41e3c68979937e963598b5ac56154
ddgo <1812805089@qq.com>
2021-12-27 23:35:52 +0800

    gengai

--- code/ddgo/Main.cpp
+++ code/ddgo/Main.cpp
@@ -16,9 +16,9 @@ int main()
             cout << "语法分析有问题, 编译停止!" << endl;
         else {
             cout<<"语法分析成功"<<endl;
-            // es = TESTmachine();
-            // if(es > 0) cout << "模拟机转换失败" <<endl;
-            // else cout << "模拟机转换成功" <<endl; 
+            es = TESTmachine();
+            if(es > 0) cout << "模拟机转换失败" <<endl;
+            else cout << "模拟机转换成功" <<endl; 
         }
     }
 
